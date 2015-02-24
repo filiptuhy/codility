815a1d55812be6695e007864fd6075b4e5c5acea
filiptuhy <filip.tuhy@gmail.com>
2015-02-24 15:21:13 +0100

    Update PermMissingElem.py

--- lesson01/PermMissingElem.py
+++ lesson01/PermMissingElem.py
@@ -4,3 +4,18 @@ def solution(A):
     b = set(B)
     result = [i for i in b if i not in a]
     return result[0]
+---------------------
+class Solution {
+    public int solution(int[] A) {
+        int araySum = 0;
+        int fullSum = 0;
+        for(int i = 0;i<A.length;i++)
+        {
+         araySum += A[i];
+         fullSum += i; 
+        }
+        fullSum += A.length;
+        fullSum += A.length+1;
+        return fullSum - araySum;
+    }
+}
